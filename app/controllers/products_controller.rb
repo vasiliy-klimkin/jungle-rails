8603bba62d451e0fba3c3794958b52d2fb241ed3
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-07-06 15:22:14 +0000

    Added so the nav bar shows the categories link

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @bobs = Product.all.order(created_at: :desc)
   end
 
   def show
